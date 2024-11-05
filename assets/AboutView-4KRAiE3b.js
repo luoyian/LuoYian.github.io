31a14dbf7ec9cbe6637cf7236ade2641d51b2d30
luoyian <mr.luoyian@gmail.com>
2024-11-05 17:24:51 +0800

    first

---
+++ assets/AboutView-4KRAiE3b.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,a as s,o as a}from"./index-CQUTGW6s.js";const n={},c={class:"about"};function r(_,e){return a(),t("div",c,e[0]||(e[0]=[s("h1",null,"LuoYian",-1)]))}const l=o(n,[["render",r]]);export{l as default};
